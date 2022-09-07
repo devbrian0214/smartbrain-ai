2cdc0c513f0e425a3882865b31e2bb09dcf3bc28
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-07 17:48:29 +0100

    implement facial recognition component with clarifai API

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import Navigation from "../components/Navigation/Navigation";
 import Rank from "../components/Rank/Rank";
 import Logo from "../components/Logo/Logo";
 import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
+import FacialRecognition from "../components/FacialRecognition/FacialRecognition";
 import BgParticles from "../components/BgParticles/BgParticles";
 import "./App.css";
 
@@ -11,17 +12,59 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
     };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log("click");
+    this.setState({ imageUrl: this.state.input });
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: process.env.REACT_APP_USER_ID,
+        app_id: process.env.REACT_APP_APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: this.state.input,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + process.env.REACT_APP_PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+        process.env.REACT_APP_MODEL_ID +
+        "/outputs",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        console.log(
+          result.outputs[0].data.regions[0]["region_info"]["bounding_box"]
+        )
+      )
+      .catch((error) => console.log("error", error));
   };
+
   render() {
+    const { imageUrl } = this.state;
     return (
       <div className="App">
         <BgParticles />
@@ -32,6 +75,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
+        {imageUrl && <FacialRecognition imageUrl={imageUrl} />}
       </div>
     );
   }
