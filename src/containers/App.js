c8a7f68264f4cffbd465b814e0fb3900f1959fee
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-07 15:01:29 +0100

    skeleton

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Navigation from "../components/Navigation/Navigation";
+import Rank from "../components/Rank/Rank";
 import Logo from "../components/Logo/Logo";
+import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
+import BgParticles from "../components/BgParticles/BgParticles";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <BgParticles />
         <Navigation />
         <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
