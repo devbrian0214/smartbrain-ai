8441426ec49f1cc52604351c87443ec29bdefdb5
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-19 15:49:55 +0100

    index.js and package

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+// Router
 import userRouter from './Routes/user.js';
+import profileRouter from './Routes/profile.js';
+import imageRouter from './Routes/image.js';
 
 //inital setup
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
 app.use(cors());
+dotenv.config();
 
 app.use('/user', userRouter);
+app.use('/profile', profileRouter);
+app.use('/imageURL', imageRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to SmartBrainAi server');
