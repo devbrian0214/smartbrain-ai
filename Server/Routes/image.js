ac4cb3d441f9656a7343fe0163afe9f027f13f8f
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-19 18:09:16 +0100

    add authorization middleware

--- Server/Routes/image.js
+++ Server/Routes/image.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-// import controller
+// import controller & middleware
 import { handleImageURL } from '../Controllers/imageURL.js';
+import { auth } from '../Middlewares/Auth.js';
 
 // create router
 const router = express.Router();
 
-router.post('/', handleImageURL);
+router.post('/', auth, handleImageURL);
 
 export default router;
--- Server/Routes/profile.js
+++ Server/Routes/profile.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-// import controller
+// import controller & middleware
 import { getProfile, updateProfileEntries } from '../Controllers/profile.js';
+import { auth } from '../Middlewares/Auth.js';
 
 // create router
 const router = express.Router();
 
-router.get('/:id', getProfile);
-router.patch('/entries', updateProfileEntries);
+router.get('/:id', auth, getProfile);
+router.patch('/entries', auth, updateProfileEntries);
 
 export default router;
