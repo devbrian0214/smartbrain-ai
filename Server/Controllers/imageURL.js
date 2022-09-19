e496dbf95e2406a94ded5d94d615fc7cfaf18142
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-19 16:45:26 +0100

    face dection gRPC server API

--- Server/Controllers/imageURL.js
+++ Server/Controllers/imageURL.js
@@ -1,3 +1,57 @@
 import { responses } from '../Common/responseAPI.js';
+import { ClarifaiStub, grpc } from 'clarifai-nodejs-grpc';
+import * as dotenv from 'dotenv';
 
-export const handleImageURL = async (req, res) => {};
+dotenv.config();
+
+const stub = ClarifaiStub.grpc();
+
+// This will be used by every Clarifai endpoint call
+const metadata = new grpc.Metadata();
+metadata.set('authorization', 'Key ' + process.env.CLA_PAT);
+
+export const handleImageURL = (req, res) => {
+  try {
+    const { imageURL } = req.body;
+
+    stub.PostModelOutputs(
+      {
+        user_app_id: {
+          user_id: process.env.CLA_USER_ID,
+          app_id: process.env.CLA_APP_ID,
+        },
+        model_id: process.env.CLA_MODEL_ID,
+        inputs: [
+          { data: { image: { url: imageURL, allow_duplicate_url: true } } },
+        ],
+      },
+      metadata,
+      (err, response) => {
+        if (err) {
+          throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+          throw new Error(
+            'Post model outputs failed, status: ' + response.status.description
+          );
+        }
+
+        // Since we have one input, one output will exist here.
+        const output = response.outputs[0].data.regions;
+
+        // extract region_info into array of regions
+        const regionArr = [];
+
+        for (let region of output) {
+          regionArr.push(region['region_info']['bounding_box']);
+        }
+
+        // send response
+        responses._200(res, { regionArr, message: 'Sucessful' });
+      }
+    );
+  } catch (error) {
+    responses._401(res, { message: 'Failed' });
+  }
+};
