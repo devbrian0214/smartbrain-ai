41812db3cf74e02f074014346080dd1528cfe3f9
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-20 14:37:43 +0100

    Rank component

--- Client/src/components/Rank/Rank.js
+++ Client/src/components/Rank/Rank.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className="f3">{"Liam, your current rank is..."}</div>
-      <div className="f1">{"5"}</div>
+      <div className="f3">{`${name}, your current rank is...`}</div>
+      <div className="f1">{`${entries}`}</div>
     </div>
   );
 };
