0df3721d0cabe144ac817a8f2b8e1ac46af1da7d
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-21 04:52:45 +0100

    develop branch

--- Server/Common/connectDB.js
+++ Server/Common/connectDB.js
@@ -3,25 +3,14 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-// uncomment to connect local database
-// export const db = knex({
-//   client: process.env.CLIENT,
-//   connection: {
-//     host: process.env.HOST,
-//     port: process.env.PORT_DB,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//   },
-// });
-
-//connect server database
+// Connect local database
 export const db = knex({
   client: process.env.CLIENT,
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    host: process.env.HOST,
+    port: process.env.PORT_DB,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
   },
 });
