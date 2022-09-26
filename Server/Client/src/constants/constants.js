a9a5e6e8acac901e6ba91a6f127e5c1716c60002
devbrian0214 <elitedevbrianh@gmail.com>
2022-09-26 17:01:15 +0100

    update server side

--- Server/Client/src/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const SIGNIN = 'SIGNIN';
-export const REGISTER = 'REGISTER';
-export const SIGNOUT = 'SIGNOUT';
-export const HOME = 'HOME';
--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 });
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Server Listening
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
